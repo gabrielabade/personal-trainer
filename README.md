a26e29d6ae4a46e81aa2f0d278a35f6866f9ceda
gabrielabade <gabriela.bade@hotmail.com>
2024-06-26 18:06:36 -0300

    personal trainer project start

---
+++ README.md
@@ -0,0 +1,26 @@
+## Ciclista Tio Pinguinho - Loja de Bicicletas peças e acessórios
+
+Este é um projeto de página web desenvolvido para ciclista tio pinguinho, apresentando serviços oferecidos e marcas com que trabalha. A página inclui formulário para pedidos de orçamento e dúvidas. Acesso rápido às redes sociais.
+
+### Funcionalidades
+
+- **Design Responsivo:** A página é projetada para se adaptar a diferentes dispositivos e tamanhos de tela, garantindo uma experiência consistente para os usuários.
+- **Interatividade:** Os usuários podem navegar pela header e visualizar mais informações sobre a empresa e também visualizar modelos de bicicletas do momento podendo alternar as cores dinamicamente na aba de catálogo.
+- **Orçamento fácil:** Botões de ação estão disponíveis para facilitar o orçamento dos serviços, com links diretos para o WhatsApp da empresa.
+- **Facilidade de navegação:** A página é organizada de forma clara e intuitiva, permitindo que os usuários encontrem facilmente as informações desejadas.
+
+ ### Como visualizar
+
+Você pode visualizar a aplicação online [aqui](https://ciclistatiopinguinho.netlify.app/)
+
+### Tecnologias utilizadas
+
+- HTML5
+- CSS3
+- JavaScript
+
+<div>
+
+![Preview do projeto](./assets/preview.gif)
+
+</div>
---
+++ script.js
@@ -0,0 +1,77 @@
+// Toggle Menu
+function toggleMenu() {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("show");
+}
+
+// Form Handling
+const form = document.querySelector(".formulario-fale-conosco");
+const background = document.querySelector(".mascara-formulario");
+
+function showForm() {
+  form.style.left = "50%";
+  form.style.transform = "translateX(-50%)";
+  background.style.visibility = "visible";
+}
+
+function hideForm() {
+  form.style.left = "-300px";
+  form.style.transform = "translateX(0)";
+  background.style.visibility = "hidden";
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const duvida = document.getElementById("duvida").value;
+
+  const mensagem = `Olá Paulo, venho através do seu site. Meu nome é ${nome}. Minha meta é: ${duvida}.`;
+  const mensagemCodificada = encodeURIComponent(mensagem);
+  const whatsappLink = `https://wa.me/5548991056014?text=${mensagemCodificada}`;
+
+  document.getElementById("whatsappLink").href = whatsappLink;
+  window.open(whatsappLink, "_blank");
+});
+//accordions session sobre
+document.addEventListener("DOMContentLoaded", function () {
+  const accordions = document.querySelectorAll(".accordion-header");
+
+  accordions.forEach(function (accordion) {
+    accordion.addEventListener("click", function () {
+      this.classList.toggle("active");
+      const content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const accordions = document.querySelectorAll(".accordion");
+
+  accordions.forEach((accordion) => {
+    accordion.addEventListener("click", () => {
+      // Fecha todos os accordions
+      accordions.forEach((item) => {
+        if (item !== accordion) {
+          item.classList.remove("active");
+          item.querySelector(".accordion-content").style.display = "none";
+        }
+      });
+
+      // Alterna o estado do accordion clicado
+      const content = accordion.querySelector(".accordion-content");
+      if (accordion.classList.contains("active")) {
+        accordion.classList.remove("active");
+        content.style.display = "none";
+      } else {
+        accordion.classList.add("active");
+        content.style.display = "block";
+      }
+    });
+  });
+});
